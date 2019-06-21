c2911b053869b4f1e9b87bf489d5d5997bb66788
chaungng <ngocchau0710@hotmail.com>
2019-06-21 01:00:04 -0700

    commit initial packages

---
+++ rmpi-tracker-service/src/main/java/com/chaungng/rmpitracker/rmpitrackerservice/RmpiTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chaungng.rmpitracker.rmpitrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmpiTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmpiTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ rmpi-tracker-service/src/test/java/com/chaungng/rmpitracker/rmpitrackerservice/RmpiTrackerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaungng.rmpitracker.rmpitrackerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RmpiTrackerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
